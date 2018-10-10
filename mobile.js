c4884e7a99f7077beb466fa665a55dff77fc0211
MAdhuri soni <soni.ranu8@gmail.com>
2018-10-10 21:11:00 +0200

    mobileapp

--- mobile.js
+++ mobile.js
@@ -1,6 +1,7 @@
 var main = new Vue({
 	el: '#main',
 	data: {
+		data: [],
 		map: [],
 		team: [],
 		schedule: [],
@@ -9,7 +10,12 @@ var main = new Vue({
 		currentMap: false,
 		currentSchedule: false,
 		currentChat: false,
-		currentTeam: false
+		currentTeam: false,
+		main:true,
+		showTeamInfo: false,
+		teamNumber: -1,
+        shwbtn:true,
+//        loading:true,
 	},
 	created: function () {
 		this.start();
@@ -17,14 +23,14 @@ var main = new Vue({
 	methods: {
 		start: function () {
 			var fetchConfig =
-				fetch("https://api.myjson.com/bins/bpmng", {
+				fetch("https://api.myjson.com/bins/8sxd0", {
 					method: "GET",
 				}).then(function (response) {
 					if (response.ok) {
 						return response.json();
 					}
 				}).then(function (json) {
-
+                    
 					var data = json;
 					console.log(data);
 					main.locations = data.schedule.locations;
@@ -33,14 +39,58 @@ var main = new Vue({
 					console.log(main.team);
 					main.schedule = data.schedule;
 					console.log(main.schedule);
+//                    main.loading=false;
+                    
+                    
 
-
-
+        
 				})
 				.catch(function (error) {
 					console.log(error);
 				})
 		},
+//		showTeamInfo: function () 
+//		{
+//			for (var i = 0; i<schedule.length; i++)
+//				{
+//					if(schedule.september[i].host == U1 || schedule.september[i].visitor == U1 )
+//					{	
+//						this.september = this.schedule.september[i].host;
+//					    this.september = this.schedule.september[i].visitor;
+//					   console.log(main.september);
+//					}
+//					 if(schedule.october.host== U1 || schedule.october.visitor == U1)  
+//						 {
+//							 main.october = data.schedule.october.host;
+//							 main.october = data.schedule.october.visitor;
+//						 console.log(main.october);
+//						 }
+//				}
+//				
+//			     if (this.teamNumber== -1 && this.showTeamInfo==false){
+        // this.teamNumber=ind;
+        // this.showTeamInfo=true
+         
+    // }
+     //else 
+//        if (this.teamNumber== ind && this.showTeamInfo==true){
+//         this.showTeamInfo=false
+//     }
+//     else{ },
+////			
+//		},
+ showteam:function(ind){
+
+         this.showTeamInfo=true;
+         this.shwbtn=false;
+         this.teamNumber=ind
+     },
+//        back:function()
+//        {
+//            this.showTeamInfo=false;
+//            this.shwbtn=true;
+//            this.teamNumber=0;
+//        }
 
 	},
 
